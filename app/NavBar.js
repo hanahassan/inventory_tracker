863ae64eceb2cb3f95249e63ad1334cb6e0cc7ac
hanahassan <hana0hassan0@gmail.com>
2024-08-03 17:51:55 +0300

    edited the nav bar

--- app/NavBar.js
+++ app/NavBar.js
@@ -1,16 +1,37 @@
 import { AppBar, Toolbar, Typography, Button, IconButton } from "@mui/material";
-import Link from 'next/link';
+import Link from "next/link";
 
 export default function NavBar() {
   return (
-    <AppBar position="static" sx={{ backgroundColor: '#464682' }}>
+    <AppBar position="static" sx={{ backgroundColor: "#464682" }}>
       <Toolbar>
-        <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-        </IconButton>
+        <IconButton
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          sx={{ mr: 2 }}
+        ></IconButton>
         <Typography variant="h6" sx={{ flexGrow: 1 }}>
           Pantry Tracker
         </Typography>
-        <Button color="inherit" component={Link} href="/">Home</Button>
+        <Button color="inherit" component={Link} href="/" sx={{ ":hover": { backgroundColor: "rgba(0, 0, 0, 0.3)", },
+          }}>
+          Home
+        </Button>
+        <Button
+          color="inherit"
+          component={Link}
+          href="https://hanahassan.netlify.app"
+          target="_blank"
+          rel="noopener noreferrer"
+          sx={{
+            ":hover": {
+              backgroundColor: "rgba(0, 0, 0, 0.3)",
+            },
+          }}
+        >
+          About Me
+        </Button>
       </Toolbar>
     </AppBar>
   );
